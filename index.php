9d7968cf420696a3e78f2d11fd2a299edcb6b213
abdulwakili <97526644+abdulwakili@users.noreply.github.com>
2022-04-09 16:35:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,35 @@
-<?php
-    /**
-     * QR Code + Logo Generator
-     *
-     * http://labs.nticompassinc.com
-     */
-    $data = $transaction_narration;
-    $size = '180x180';
-    $logo = 'img/logo.png';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QR Code Styling</title>
+    <script type="text/javascript" src="https://unpkg.com/qr-code-styling@1.5.0/lib/qr-code-styling.js"></script>
+</head>
+<body>
+<div id="canvas" style="background: white;padding: 12px;width: 300px;border: 6px solid #2575e1;border-radius: 20px;"></div>
+<script type="text/javascript">
 
+    const qrCode = new QRCodeStyling({
+        width: 300,
+        height: 300,
+        type: "svg",
+        data: "https://github.com/abdulwakili/",
+        image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+        dotsOptions: {
+            color: "#2575e1",
+            type: "rounded"
+        },
+        backgroundOptions: {
+            color: "#ffffff",
+        },
+        imageOptions: {
+            crossOrigin: "anonymous",
+            margin: 10
+        }
+    });
 
-    header('Content-type: image/png');
-    // Get QR Code image from Google Chart API
-    // http://code.google.com/apis/chart/infographics/docs/qr_codes.html
-    $QR = imagecreatefrompng('https://chart.googleapis.com/chart?cht=qr&chld=H|1&chs='.$size.'&chl='.urlencode($data));
-    if($logo !== FALSE){
-        $logo = imagecreatefromstring(file_get_contents($logo));
-
-        $QR_width = imagesx($QR);
-        $QR_height = imagesy($QR);
-
-        $logo_width = imagesx($logo);
-        $logo_height = imagesy($logo);
-
-        // Scale logo to fit in the QR Code
-        $logo_qr_width = $QR_width/3;
-        $scale = $logo_width/$logo_qr_width;
-        $logo_qr_height = $logo_height/$scale;
-
-        imagecopyresampled($QR, $logo, $QR_width/3, $QR_height/3, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
-
-    }
-    //imagepng($QR, 'qrcode.png');
-    // Ouput image in the browser
-    //echo '<img src="../qrcode.png">';
-
-    ob_start();
-    imagepng($QR);
-    $QR = ob_get_contents();
-    ob_end_clean();
-    echo '<img src="data:image/png;base64,'.base64_encode($QR).'" class="mx-auto d-block" style=" width: 200px; border-radius: 20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border: 4px solid #2374e1; "/>';
-
-    imagedestroy($QR);
-
-  ?>
+    qrCode.append(document.getElementById("canvas"));
+    qrCode.download({ name: "qr", extension: "svg" });
+</script>
+</body>
+</html>
